baddce7b6b90dffb743f5f38e7ef3accb1b260d5
M <michaelandrewlongoria@gmail.com>
2020-01-15 11:41:19 -0800

    Create solution2.js

---
+++ src/algorithms/arrays/two-number-sum/solution2.js
@@ -0,0 +1,20 @@
+// O(N) time | O(N) space
+
+function twoNumberSum(array, targetSum) {
+	const nums = {};
+  
+	for (const num of array) {
+		const potentialMatch = targetSum - num;
+    
+		if (potentialMatch in nums) {
+			return [num, potentialMatch];
+		} else {
+			nums[num] = true;
+		}
+	}
+	
+	return [];
+}
+
+// Do not edit the line below.
+exports.twoNumberSum = twoNumberSum;
