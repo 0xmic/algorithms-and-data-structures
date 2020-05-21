1e9ca1724d37d23ac2563a1e877107fe26457ad6
M Longoria <michaelandrewlongoria@gmail.com>
2020-05-21 01:20:18 -0700

    udpate algo/find-three-largest-numbers w/ solution

--- src/algorithms/searching/find-three-largest-numbers/solution.py
+++ src/algorithms/searching/find-three-largest-numbers/solution.py
@@ -1,4 +1,23 @@
+# O(N) time | O(1) space
 def findThreeLargestNumbers(array):
-    # TODO
-    
-    pass
+    threeLargest = [None, None, None]
+    for num in array:
+    	updateLargest(threeLargest, num)
+    return threeLargest
+
+
+def updateLargest(threeLargest, num):
+	if threeLargest[2] is None or num > threeLargest[2]:
+		shiftAndUpdate(threeLargest, num, 2)
+	elif threeLargest[1] is None or num > threeLargest[1]:
+		shiftAndUpdate(threeLargest, num, 1)
+	elif threeLargest[0] is None or num > threeLargest[0]:
+		shiftAndUpdate(threeLargest, num, 0)
+
+
+def shiftAndUpdate(array, num, idx):
+	for i in range(idx + 1):
+		if i == idx:
+			array[i] = num
+		else:
+			array[i] = array[i + 1]
