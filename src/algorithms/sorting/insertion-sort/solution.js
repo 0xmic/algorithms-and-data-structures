fdae764781d1df7bba095a6785a44c0a286fd4db
M Longoria <michaelandrewlongoria@gmail.com>
2020-05-23 12:16:49 -0700

    update algo/insertion-sort w/ solution

--- src/algorithms/sorting/insertion-sort/solution.js
+++ src/algorithms/sorting/insertion-sort/solution.js
@@ -1,5 +1,19 @@
+// O(n^2) time | O(1) space
 function insertionSort(array) {
-  // TODO
+  for (let i = 1; i < array.length; i++) {
+  	let j = i;
+  	while (j > 0 && array[j] < array[j - 1]) {
+  		swap(j, j - 1, array);
+  		j -= 1;
+  	}
+  }
+  return array;
+}
+
+function swap(i, j, array) {
+	const temp = array[j];
+	array[j] = array[i];
+	array[i] = temp;
 }
 
 exports.insertionSort = insertionSort;
--- src/algorithms/sorting/insertion-sort/solution.py
+++ src/algorithms/sorting/insertion-sort/solution.py
@@ -1,3 +1,11 @@
+# O(n^2) time | O(1) space
 def insertionSort(array):
-  # TODO
-  pass
+  for i in range(1, len(array)):
+    j = i
+    while j > 0 and array[j] < array[j - 1]:
+      swap(j, j - 1, array)
+      j -= 1
+  return array
+
+def swap(i, j, array):
+  array[i], array[j] = array[j], array[i]
