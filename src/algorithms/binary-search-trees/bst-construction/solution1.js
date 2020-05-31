eb2296fbddd96d325053e4ed5af3267dc156c5d4
M Longoria <michaelandrewlongoria@gmail.com>
2020-05-30 21:00:46 -0700

    create README and solution templates for algo/bst-construction

---
+++ src/algorithms/binary-search-trees/bst-construction/solution1.js
@@ -0,0 +1,24 @@
+// O( ) time | O( ) space
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    // TODO
+    return this;
+  }
+
+  contains(value) {
+    // TODO
+  }
+
+  remove(value) {
+    // TODO
+    return this;
+  }
+}
+
+exports.BST = BST;
---
+++ src/algorithms/binary-search-trees/bst-construction/solution2.js
@@ -0,0 +1,23 @@
+// O( ) time | O( ) space
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    // TODO
+    return this;
+  }
+
+  contains(value) {
+    // TODO
+  }
+
+  remove(value) {
+    // TODO
+    return this;
+  }
+}
+
