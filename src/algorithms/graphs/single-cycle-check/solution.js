b0d5b31ad39bfc5c4aaaf42d7e27d12051269aa3
M Longoria <michaelandrewlongoria@gmail.com>
2020-06-22 19:54:55 -0700

    update README and solutions

--- src/algorithms/graphs/single-cycle-check/solution.js
+++ src/algorithms/graphs/single-cycle-check/solution.js
@@ -1,5 +1,19 @@
+// O(n) time | O(1) space
 function hasSingleCycle(array) {
-	// TODO
+	let numElementsVisited = 0;
+	let currentIdx = 0;
+	while (numElementsVisited < array.length) {
+		if (numElementsVisited > 0 && currentIdx === 0) return false;
+		numElementsVisited++;
+		currentIdx = getNextIdx(currentIdx, array);
+	}
+	return currentIdx === 0;
+}
+
+function getNextIdx(currentIdx, array) {
+	const jump = array[currentIdx];
+	const nextIdx = (currentIdx + jump) % array.length;
+	return nextIdx >= 0 ? nextIdx : nextIdx + array.length;
 }
 
 exports.hasSingleCycle = hasSingleCycle;
--- src/algorithms/graphs/single-cycle-check/solution.py
+++ src/algorithms/graphs/single-cycle-check/solution.py
@@ -1,3 +1,15 @@
+# O(n) time | O(1) space
 def hasSingleCycle(array):
-	# TODO
-	pass
+	numElementsVisited = 0
+	currentIdx = 0
+	while numElementsVisited < len(array):
+		if numElementsVisited > 0 and currentIdx == 0:
+			return False
+		numElementsVisited += 1
+		currentIdx = getNextIdx(currentIdx, array)
+	return currentIdx == 0
+
+def getNextIdx(currentIdx, array):
+	jump = array[currentIdx]
+	nextIdx = (currentIdx + jump) % len(array)
+	return nextIdx if nextIdx >= 0 else nextIdx + len(array)
