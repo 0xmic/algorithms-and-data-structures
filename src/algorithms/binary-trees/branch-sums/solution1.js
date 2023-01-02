18cca4620b2d44a649e5be54aeb8a272e21a0e5d
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-01-01 20:49:34 -0800

    Update page for Branch Sums

--- src/algorithms/binary-trees/branch-sums/solution1.js
+++ src/algorithms/binary-trees/branch-sums/solution1.js
@@ -11,14 +11,14 @@ class BinaryTree {
         this.value = value;
         this.left = null;
         this.right = null;
-    }
-}
+    };
+};
 
 function branchSums(root) {
     const sums = [];
     calculateBranchSums(root, 0, sums);
     return sums;
-}
+};
 
 function calculateBranchSums(node, runningSum, sums) {
     if (!node) return;
@@ -28,11 +28,11 @@ function calculateBranchSums(node, runningSum, sums) {
     if (!node.left && !node.right) {
         sums.push(newRunningSum);
         return;
-    }
+    };
 
     calculateBranchSums(node.left, newRunningSum, sums);
     calculateBranchSums(node.right, newRunningSum, sums);
-}
+};
 
 exports.BinaryTree = BinaryTree;
 exports.branchSums = branchSums;
