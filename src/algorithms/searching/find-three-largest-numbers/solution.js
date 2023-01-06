66f0787477340c9f596a0e0fb780319406578ae1
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-01-06 14:45:37 -0800

    Update Find Three Largest Numbers

--- src/algorithms/searching/find-three-largest-numbers/solution.js
+++ src/algorithms/searching/find-three-largest-numbers/solution.js
@@ -2,9 +2,9 @@ function findThreeLargestNumbers(array) {
   const threeLargest = [null, null, null];
   for (const num of array) {
   	updateLargest(threeLargest, num);
-  }
+  };
   return threeLargest;
-}
+};
 
 function updateLargest(threeLargest, num) {
 	if (threeLargest[2] === null || num > threeLargest[2]) {
@@ -13,8 +13,8 @@ function updateLargest(threeLargest, num) {
 		shiftAndUpdate(threeLargest, num, 1);
 	} else if (threeLargest[0] === null || num > threeLargest[0]) {
 		shiftAndUpdate(threeLargest, num, 0);
-	}
-}
+	};
+};
 
 function shiftAndUpdate(array, num, idx) {
 	for (let i = 0; i <= idx; i++) {
@@ -22,8 +22,8 @@ function shiftAndUpdate(array, num, idx) {
 			array[i] = num;
 		} else {
 			array[i] = array[i + 1];
-		}
-	}
-}
+		};
+	};
+};
 
 exports.findThreeLargestNumbers = findThreeLargestNumbers;
