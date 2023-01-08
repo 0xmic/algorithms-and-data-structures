b21da4eb0e8bca4a3c86268c5ec3a062a5c21683
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-01-08 08:09:31 -0800

    Update Insertion Sort

--- src/algorithms/sorting/insertion-sort/solution.js
+++ src/algorithms/sorting/insertion-sort/solution.js
@@ -5,15 +5,15 @@ function insertionSort(array) {
   	while (j > 0 && array[j] < array[j - 1]) {
   		swap(j, j - 1, array);
   		j -= 1;
-  	}
-  }
+  	};
+  };
   return array;
-}
+};
 
 function swap(i, j, array) {
 	const temp = array[j];
 	array[j] = array[i];
 	array[i] = temp;
-}
+};
 
 exports.insertionSort = insertionSort;
