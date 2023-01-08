469ecbc15e7fb1b449fe604df2652b226071d590
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-01-07 20:31:18 -0800

    Update Bubble Sort

--- src/algorithms/sorting/bubble-sort/solution.js
+++ src/algorithms/sorting/bubble-sort/solution.js
@@ -8,17 +8,17 @@ function bubbleSort(array) {
   		if (array[i] > array[i + 1]) {
   			swap(i, i + 1, array);
   			isSorted = false;
-  		}
-  	}
+  		};
+  	};
   	counter++;
-  }
+  };
   return array;
-}
+};
 
 function swap(i, j, array) {
 	const temp = array[j];
 	array[j] = array[i];
 	array[i] = temp;
-}
+};
 
 exports.bubbleSort = bubbleSort;
