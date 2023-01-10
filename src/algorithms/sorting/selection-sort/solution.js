54b4f180526170a3f3097d325dc1d2b61ca509b4
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-01-09 20:18:40 -0800

    Update Selection Sort

--- src/algorithms/sorting/selection-sort/solution.js
+++ src/algorithms/sorting/selection-sort/solution.js
@@ -5,17 +5,17 @@ function selectionSort(array) {
   	let smallestIdx = startIdx;
   	for (let i = startIdx + 1; i < array.length; i++) {
   		if (array[smallestIdx] > array[i]) smallestIdx = i
-  	}
+  	};
   	swap(startIdx, smallestIdx, array);
   	startIdx++;
-  }
+  };
   return array;
-}
+};
 
 function swap(i, j, array) {
 	const temp = array[j];
 	array[j] = array[i];
 	array[i] = temp;
-}
+};
 
 exports.selectionSort = selectionSort;
