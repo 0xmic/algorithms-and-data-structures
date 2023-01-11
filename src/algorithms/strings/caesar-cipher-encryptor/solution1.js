b1922276560acf2298ec407d403192b04da6cc3b
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-01-11 10:32:56 -0800

    Update Caesar Cipher Encryptor

--- src/algorithms/strings/caesar-cipher-encryptor/solution1.js
+++ src/algorithms/strings/caesar-cipher-encryptor/solution1.js
@@ -4,13 +4,13 @@ function caesarCipherEncryptor(string, key) {
 	const newKey = key % 26;
 	for (const letter of string) {
 		newLetters.push(getNewLetter(letter, newKey));
-	}
+	};
 	return newLetters.join('');
-}
+};
 
 function getNewLetter(letter, key) {
 	const newLetterCode = letter.charCodeAt() + key;
 	return newLetterCode <= 122 ? String.fromCharCode(newLetterCode) : String.fromCharCode(96 + (newLetterCode % 122));
-}
+};
 
 exports.caesarCipherEncryptor = caesarCipherEncryptor;
--- src/algorithms/strings/caesar-cipher-encryptor/solution2.js
+++ src/algorithms/strings/caesar-cipher-encryptor/solution2.js
@@ -5,13 +5,13 @@ function caesarCipherEncryptor(string, key) {
 	const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 	for (const letter of string) {
 		newLetters.push(getNewLetter(letter, newKey, alphabet));
-	}
+	};
 	return newLetters.join('');
-}
+};
 
 function getNewLetter(letter, key, alphabet) {
 	const newLetterCode = alphabet.indexOf(letter) + key;
 	return newLetterCode <= 25 ? alphabet[newLetterCode] : alphabet[-1 + (newLetterCode % 25)];
-}
+};
 
 exports.caesarCipherEncryptor = caesarCipherEncryptor;
