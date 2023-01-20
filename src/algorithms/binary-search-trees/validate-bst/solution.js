3aba959d1ce1f94d37d99fac018d3330a7e5f82b
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-01-19 23:44:20 -0800

    Update Validate BST

--- src/algorithms/binary-search-trees/validate-bst/solution.js
+++ src/algorithms/binary-search-trees/validate-bst/solution.js
@@ -6,12 +6,12 @@ class BST {
     this.value = value;
     this.left = null;
     this.right = null;
-  }
-}
+  };
+};
 
 function validateBst(tree) {
   return validateBstHelper(tree, -Infinity, Infinity);
-}
+};
 
 function validateBstHelper(tree, minValue, maxValue) {
 	if (tree === null) return true;
@@ -19,7 +19,7 @@ function validateBstHelper(tree, minValue, maxValue) {
 	const leftIsValid = validateBstHelper(tree.left, minValue, tree.value);
 	const rightIsValid = validateBstHelper(tree.right, tree.value, maxValue);
 	return leftIsValid && rightIsValid;
-}
+};
 
 exports.BST = BST;
 exports.validateBst = validateBst;
