b1b9b3407c3a20058733b1a0d68c7d28fbed2d3f
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-02-06 22:45:56 -0800

    Update Remove Kth Node from End

--- src/algorithms/linked-lists/remove-kth-node-from-end/solution.js
+++ src/algorithms/linked-lists/remove-kth-node-from-end/solution.js
@@ -2,8 +2,8 @@ class LinkedList {
   constructor(value) {
     this.value = value;
     this.next = null;
-  }
-}
+  };
+};
 
 // O(n) time | O(1) space
 function removeKthNodeFromEnd(head, k) {
@@ -13,18 +13,18 @@ function removeKthNodeFromEnd(head, k) {
   while (counter <= k) {
   	second = second.next;
   	counter++;
-  }
+  };
   if (second === null) {
   	head.value = head.next.value;
   	head.next = head.next.next;
   	return;
-  }
+  };
   while (second.next !== null) {
   	second = second.next;
   	first = first.next;
-  }
+  };
   first.next = first.next.next;
-}
+};
 
 exports.LinkedList = LinkedList;
 exports.removeKthNodeFromEnd = removeKthNodeFromEnd;
