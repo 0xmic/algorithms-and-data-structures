7f734c3ce6de72fad4492f9eebc16657ba003828
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-02-11 08:59:35 -0800

    Update Min Max Stack Construction

--- src/algorithms/stacks/min-max-stack-construction/solution.js
+++ src/algorithms/stacks/min-max-stack-construction/solution.js
@@ -2,18 +2,18 @@ class MinMaxStack {
 	constructor() {
 		this.minMaxStack = [];
 		this.stack = [];
-	}
+	};
 
 	// O(1) time | O(1) space
 	peek() {
 		return this.stack[this.stack.length - 1];
-	}
+	};
 
 	// O(1) time | O(1) space
 	pop() {
 		this.minMaxStack.pop();
 		return this.stack.pop();
-	}
+	};
 
 	// O(1) time | O(1) space
 	push(number) {
@@ -25,17 +25,17 @@ class MinMaxStack {
 		}
 		this.minMaxStack.push(newMinMax);
 		this.stack.push(number);
-	}
+	};
 
 	// O(1) time | O(1) space
 	getMin() {
 		return this.minMaxStack[this.minMaxStack.length - 1].min;
-	}
+	};
 
 	// O(1) time | O(1) space
 	getMax() {
 		return this.minMaxStack[this.minMaxStack.length - 1].max;
-	}
-}
+	};
+};
 
 exports.MinMaxStack = MinMaxStack;
