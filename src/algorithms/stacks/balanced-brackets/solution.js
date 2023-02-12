25ae0df38bf83fdc4fd735adc9936679d1285d44
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-02-12 11:51:33 -0800

    Update Balanced Brackets

--- src/algorithms/stacks/balanced-brackets/solution.js
+++ src/algorithms/stacks/balanced-brackets/solution.js
@@ -10,15 +10,15 @@ function balancedBrackets(string) {
 		} else if (closingBrackets.includes(char)) {
 			if (stack.length == 0) {
 				return false;
-			}
+			};
 			if (stack[stack.length - 1] === matchingBrackets[char]) {
 				stack.pop();
 			} else {
 				return false;
-			}
-		}
-	}
+			};
+		};
+	};
 	return stack.length === 0;
-}
+};
 
 exports.balancedBrackets = balancedBrackets;
