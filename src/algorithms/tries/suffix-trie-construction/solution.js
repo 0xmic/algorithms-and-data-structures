488a4ad5799264389f950e6702d9f209042aca73
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-02-17 20:20:17 -0800

    Update Suffix Trie Construction

--- src/algorithms/tries/suffix-trie-construction/solution.js
+++ src/algorithms/tries/suffix-trie-construction/solution.js
@@ -3,14 +3,14 @@ class SuffixTrie {
 		this.root = {};
 		this.endSymbol = '*';
 		this.populateSuffixTrieFrom(string);
-	}
+	};
 
 	// O(n^2) time | O(n^2) space
 	populateSuffixTrieFrom(string) {
 		for (let i = 0; i < string.length; i++) {
 			this.insertSubstringStartingAt(i, string);
-		}
-	}
+		};
+	};
 
 	insertSubstringStartingAt(i, string) {
 		let node = this.root;
@@ -18,9 +18,9 @@ class SuffixTrie {
 			const letter = string[j];
 			if (!(letter in node)) node[letter] = {};
 			node = node[letter];
-		}
+		};
 		node[this.endSymbol] = true;
-	}
+	};
 
 	// O(m) time | O(1) space
 	contains(string) {
@@ -28,9 +28,9 @@ class SuffixTrie {
 		for (const letter of string) {
 			if (!(letter in node)) return false;
 			node = node[letter];
-		}
+		};
 		return this.endSymbol in node;
-	}
-}
+	};
+};
 
 exports.SuffixTrie = SuffixTrie;
