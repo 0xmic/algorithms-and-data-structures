46dca6a6244bb3321d88dfcccafd21a4119d898b
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-04-15 02:01:08 +0000

    Add Run-Length Encoding - TypeScript

---
+++ src/algorithms/strings/run-length-encoding/solution.ts
@@ -0,0 +1,27 @@
+// O(n) Time | O(n) Space
+// n = length of the input string
+export function runLengthEncoding(string: string) {
+  // The input string is guaranteed to be non-empty,
+  // so our first run will be of at least length 1.
+  const encodedStringCharacters: string[] = [];
+  let currentRunLength = 1;
+
+  for (let i = 1; i < string.length; i++) {
+    const currentCharacter = string[i];
+    const previousCharacter = string[i - 1];
+
+    if (currentCharacter !== previousCharacter || currentRunLength === 9) {
+      encodedStringCharacters.push(currentRunLength.toString());
+      encodedStringCharacters.push(previousCharacter);
+      currentRunLength = 0;
+    }
+
+    currentRunLength++;
+  }
+
+  // Handle the last run.
+  encodedStringCharacters.push(currentRunLength.toString());
+  encodedStringCharacters.push(string[string.length - 1]);
+
+  return encodedStringCharacters.join("");
+}
