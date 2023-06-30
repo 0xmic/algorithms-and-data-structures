e7c9e1f12d021d66fb8a5df00a9c00f2a953e562
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-06-29 18:56:10 -0700

    Adds Number of Ways to Make Change - TypeScript

---
+++ src/algorithms/dynamic-programming/number-of-ways-to-make-change/solution.ts
@@ -0,0 +1,13 @@
+// O(nd) time | O(n) space
+export function numberOfWaysToMakeChange(n: number, denoms: number[]) {
+  const ways = new Array(n + 1).fill(0);
+  ways[0] = 1;
+  for (let denom of denoms) {
+  	for (let amount = 1; amount < n + 1; amount++) {
+  		if (denom <= amount) {
+  			ways[amount] += ways[amount - denom];
+  		};
+  	};
+  };
+  return ways[n];
+};
